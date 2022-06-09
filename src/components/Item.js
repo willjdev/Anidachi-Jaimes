47cf7abadf73941911b36533c20a9a18722d7601
William Jaimes <william.jaimesfontalvo@gmail.com>
2022-06-09 13:00:37 -0500

    Completado de ItemList.js, Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function Item({pokeImagen, pokeNombre, pokePeso}) {
+export default function Item({pokeImagen, pokeNombre, pokeDescripcion}) {
   return (
-    <div className='flex flex-col items-center bg-indigo-200 rounded overflow-hidden shadow-sm w-32 h-48'>
-        <img src='{pokeImagen}' className='w-full h-3/5 sm:h-48 object-cover' alt='Imagen pokemon'/>
-        <div className='w-full h-auto m-4 flex flex-col items-center justify-evenly bg-white'>
-            <span className='font-bold text-base'>{pokeNombre}</span>
-            <span className='text-black text-sm'>Peso: {pokePeso}</span>
+    <div className='flex flex-col items-center bg-white rounded overflow-hidden shadow-lg w-52 h-80'>
+        <img src={pokeImagen} className='w-full h-3/5 sm:h-48 object-cover' alt='Imagen pokemon'/>
+        <div className='w-full h-2/5 flex flex-col items-center justify-evenly bg-indigo-100'>
+            <span className='font-medium text-base'>{pokeNombre}</span>
+            <span className='text-black text-sm text-center px-2'>{pokeDescripcion}</span>
         </div>
     </div>
   )
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Item from './Item'
 export default function ItemList({pokemon}) {
   return (
-    <div className='flex flex-row flex-wrap items-center justify-around gap-3 max-w-2xl my-3 px-2'>
-        {pokemon?.map(poke => <Item key={poke.id}  pokeNombre={poke.name}  />)}
+    <div className='flex flex-row flex-wrap items-center justify-around gap-3 max-w-5xl my-3 px-2'>
+        {pokemon?.map(poke => <Item key={poke.id}  pokeNombre={poke.name}  pokeDescripcion={poke.description} pokeImagen={poke.image}/>)}
     </div>
 
   )
